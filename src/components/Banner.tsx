27677dc1384f0348ac5e85b07c87be294e45f503
Ainun Nafiah <ainunnafiah354@gmail.com>
2024-04-25 21:46:59 +0700

    ⭐create banner component

---
+++ src/components/Banner.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Banner = () => {
+  return (
+    <>
+      <div className="">
+        <img src="new-banner.png" className="" alt="" />
+      </div>
+    </>
+  );
+};
+
+export default Banner;
