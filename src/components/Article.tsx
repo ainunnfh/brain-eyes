a1064136dbde3566042cda178847917b321f6d34
Ainun Nafiah <ainunnafiah354@gmail.com>
2024-04-25 21:45:51 +0700

    🆙Update (Article): add content to website

---
+++ src/components/Article.tsx
@@ -0,0 +1,135 @@
+import React from "react";
+
+const Article = () => {
+  return (
+    <div className="flex flex-col px-4 lg:px-0 mt-10 text-justify">
+      <p className="pb-6">
+        Saat ini dunia memiliki pesatnya perkembangan teknologi yang tak
+        terbayangkan. Anak-anak hingga orang dewasa tampaknya tak bisa lepas
+        dari akses teknologi yang semakin memanjakan penggunanya. Hampir
+        mustahil saat ini melihat orang berjalan tanpa membawa ponsel pintar
+        atau gadget. Mudahnya mendapatkan informasi dan hiburan menjadi alasan
+        orang-orang tak bisa lepas dari gadget. Namun tentu saja teknologi
+        memiliki segudang manfaat dan peran untuk dunia pendidikan berinovasi.
+      </p>
+
+      <p className="pb-6">
+        Kunci dalam membuka potensi generasi masa depan yang lebih maju adalah
+        dengan memanfaatkan teknologi. Saat ini kita dikelilingi begitu banyak
+        kemudahan berkat perkembangan teknologi yang beragam. Teknologi saat ini
+        bisa memberikan dampak besar untuk efektivitas dalam proses belajar
+        sehingga hal ini tak dapat dipungkiri memiliki potensi generasi masa
+        depan akan lebih maju dalam berbagai bidang dibandingkan generasi
+        sebelumnya.
+      </p>
+
+      <p className="pb-6">
+        Inovasi teknologi perlu dilakukan dengan cara konsisten guna menciptakan
+        sistem yang berkualitas dan efektif. Teknologi memberikan akses pada
+        sumber daya pendidikan yang besar dengan tidak terbatasnya pengetahuan
+        sehingga tak bisa dipungkiri teknologi memiliki peran penting dalam
+        pendidikan pada zaman ini. Maka setiap pendidikan pada saat ini perlu
+        memanfaatkan teknologi, bekerja sama dengan teknologi dan memfasilitasi
+        pendidikan dengan teknologi.
+      </p>
+
+      <p className="pb-6">
+        Maka bersama KUMON CONNECT yaitu platform digital yang dibuat untuk
+        mendekatkan anak-anak, orang tua dan pembimbing belajar. Mempermudah
+        anak-anak untuk mengakses pendidikannya dimana saja dan kapanpun mereka
+        mau. Pengembangan metode belajar yang mempermudah dari kumon ini tentu
+        sangat efektif dan menjadi revolusi bagi pendidikan anak. KUMON CONNECT
+        juga mendukung pendidikan dengan mengatasi kurangnya tenaga pendidik dan
+        mempermudah mereka dengan memantaunya dari balik layar yang
+        menghubungkan anak dengan orang tua mereka.
+      </p>
+      <p className="pb-6">
+        Sejak dulu, KUMON memiliki sistem yang unggul dan terstruktur untuk
+        memfasilitasi pembelajaran yang produktif. Sistem yang dirancang agar
+        secara spesifik menjangkau tingkatan umur anak sehingga mendorong anak
+        dan memotivasi mereka agar mampu bersaing.
+      </p>
+      <p className="pb-6">
+        Pendidikan dari sedini mungkin sangat memungkinkan seseorang survive
+        untuk kehidupan di masa depan. Berkat pendidikan yang tepat, seseorang
+        dapat membuka potensi yang tersembunyi dan dapat membantu mengembangkan
+        kemampuan mereka menjadi individu yang produktif.
+      </p>
+      <p className="pb-6">
+        Seperti program unggulan yang dimiliki KUMON CONNECT, yaitu :
+        <p>
+          Matematika, Program keunggulan ini memiliki sistem level untuk
+          mengembangkan kemampuan untuk para siswa dalam menghitung di tingkat
+          tinggi.
+        </p>
+        <p>
+          Bahasa Inggris, Program unggulan yang memiliki tujuan dalam
+          meningkatkan dan memperkaya siswa dalam memahami tata bahasa baru.
+        </p>
+      </p>
+      <p className="pb-6">
+        Dua program keunggulan milik KUMON CONNECT di atas merupakan salah satu
+        bentuk KUMON CONNECT merevolusi masa depan pendidikan dengan kemampuan
+        berbahasa asing dan juga mengasah logika dengan matematika.
+      </p>
+
+      <div className=" p-4">
+        <div className="pb-6 font-bold text-2xl">
+          KENAPA BEGITU PENTING MEMAHAMI BAHASA ASING DAN MATEMATIKA?
+        </div>
+        <div className="pb-6">
+          <p className="font-semibold">1. Mendukung pengembangan diri</p>
+          <p className="ml-4">
+            Memiliki kemampuan untuk memahami bahasa asing terutama bahasa
+            inggris yang saat ini menjadi bahasa internasional adalah kunci
+            untuk membuka banyak pengetahuan yang lebih luas daripada seseorang
+            yang tidak memilikinya. perspektif yang luas ini mendukung
+            pengembangan diri hingga kita dapat mengeksplorasi potensi lebih
+            dalam. Begitu Pula dengan memiliki kemampuan dalam berhitung dari
+            matematika sehingga seseorang dapat mengatasi masalah komplek dengan
+            pola berfikir yang terlatih logis dan kritis.
+          </p>
+        </div>
+
+        <div className="pb-6">
+          <p className="font-semibold">
+            2. Membantu mengembangkan kualitas hidup
+          </p>
+          <p className="ml-4">
+            Mendapatkan pemahaman yang lebih luas dari kemampuan berbahasa asing
+            dan menghitung, tentu memiliki pengaruh dalam menyerap ilmu
+            pengetahuan. Sehingga seseorang yang memiliki kemampuan tersebut
+            akan lebih unggul dibandingkan individu lain dengan begitu kualitas
+            hidupnya akan meningkat.
+          </p>
+        </div>
+        <div className="pb-6">
+          <p className="font-semibold">
+            3. Memiliki kemampuan survive menakjubkan
+          </p>
+          <p className="ml-4">
+            Seseorang yang dapat memahami bahasa asing lebih mudah beradaptasi
+            dengan lingkungan baru. Sebagai individu kita dapat cepat memahami
+            lingkungan dengan kemampuan tersebut. Ditambah lagi dengan kemampuan
+            dalam berhitung. Terasahnya logika dan berpikir kritis menjadi combo
+            yang menakjubkan sehingga kita dapat memperhitungkan banyak
+            kemungkinan sebelum mengambil keputusan.
+          </p>
+        </div>
+
+        <div>
+          Mudahnya mendapatkan informasi dan hiburan menjadi alasan orang-orang
+          tak bisa lepas dari gadget. Maka pemanfaatan teknologi bersama KUMON
+          CONNECT dapat mempermudah belajar, pengembangan pribadi lewat program
+          keunggulan kursus bahasa inggris online dan les matematika online.
+          Revolusi masa depan lebih tertata bersama KUMON Digital. Karena
+          pendidikan yang tepat, dapat membuka potensi yang tersembunyi dan
+          dapat membantu mengembangkan kemampuan mereka menjadi individu yang
+          produktif, kreatif dan unggul.
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Article;
