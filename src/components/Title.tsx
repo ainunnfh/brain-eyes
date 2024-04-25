b46baf3257a9e1c52bc0e48866abff4abd2141f9
Ainun Nafiah <ainunnafiah354@gmail.com>
2024-04-25 21:47:34 +0700

    ⭐new title component

---
+++ src/components/Title.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const Title = () => {
+  return <div className="text-3xl font-bold mb-8"
+  >MEREVOLUSI MASA DEPAN ANAK BERSAMA KUMON CONNECT</div>;
+};
+
+export default Title;
