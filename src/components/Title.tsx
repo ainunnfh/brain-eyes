26a49d58888d615d723b2df58e715b6969bb9f01
Ainun Nafiah <ainunnafiah354@gmail.com>
2024-04-25 22:28:29 +0700

    💄Update (Title): change text color

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,8 +1,13 @@
 import React from "react";
 
 const Title = () => {
-  return <div className="text-3xl font-bold mb-8"
-  >MEREVOLUSI MASA DEPAN ANAK BERSAMA KUMON CONNECT</div>;
+  return (
+    <div className="text-3xl font-bold mb-8">
+      MEREVOLUSI MASA DEPAN ANAK BERSAMA {" "}
+      <span className="text-[#7ECDF4]">KUMON </span>
+      <span className="text-[#F7B531]">CONNECT</span>
+    </div>
+  );
 };
 
 export default Title;
